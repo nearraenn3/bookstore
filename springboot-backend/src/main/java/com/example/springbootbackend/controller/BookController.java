cb91d7fefb08b7ab9c7e2791649ab33418abf8c5
nearraenn3 <nearrpk@hotmail.com>
2020-12-29 16:23:42 +0700

    [ADDED] - getBookById and updateBook REST API

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/BookController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/BookController.java
@@ -1,8 +1,10 @@
 package com.example.springbootbackend.controller;
 
+import com.example.springbootbackend.exception.ResourceNotFoundException;
 import com.example.springbootbackend.model.Book;
 import com.example.springbootbackend.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +26,35 @@ public class BookController {
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    // get employee by id rest api
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not exist with id :" + id));
+        return ResponseEntity.ok(book);
+    }
+
+    // update employee rest api
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not exist with id :" + id));
+
+        if (bookDetails.getBookName() != null) {
+            book.setBookName(bookDetails.getBookName());
+        }
+        if (bookDetails.getAuthor() != null) {
+            book.setAuthor(bookDetails.getAuthor());
+        }
+        if (bookDetails.getPrice() != 0) {
+            book.setPrice(bookDetails.getPrice());
+        }
+
+        Book updatedBook = bookRepository.save(book);
+        return ResponseEntity.ok(updatedBook);
     }
 
 }
